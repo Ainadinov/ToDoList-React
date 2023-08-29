41190c5910037538123f178b5a1700abcb0aa2c5
Ainadinov01~ <a.adilet.97@mail.ru>
2023-08-30 00:21:07 +0600

    change btn

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,19 @@ import { useState } from "react";
 import {PLUS__ICON} from "../images/index.js";
 import Modal from './Modal';
 
+const  btn = [
+    { 
+        "name": "To Do" ,
+        "status": true},
+    { 
+        "name": "Done" ,
+        "status": false},
+    { 
+        "name": "Trash" ,
+        "status": "trash"},
+
+  ];
+
 function Header({todo, setTodo, todoFilter}){
     const [isModalOpen, setModalOpen] = useState(false);
 
@@ -17,9 +30,11 @@ function Header({todo, setTodo, todoFilter}){
                 <h1 className="todo__title">Simple To Do List</h1>
                 <p className="todo__text">Today is awesome day. The weather is awesome, you are awesome too! </p>
                 <div className="todo__button">
-                    <button className="todo__bth" onClick={()=>todoFilter(true)}>To Do</button>
-                    <button className="todo__bth" onClick={()=>todoFilter(false)}>Done</button>
-                    <button className="todo__bth" onClick={()=>todoFilter("trash")}>Trash</button>
+                    {
+                        btn.map((obj, i)=>(
+                            <button onClick={()=>todoFilter(obj.status)} className="todo__bth">{obj.name}</button>
+                        ))
+                    }
                 </div>
             </div>
             <div className="header__modal">
